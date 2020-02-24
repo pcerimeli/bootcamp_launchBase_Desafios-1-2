902b28a899a0957bebb31adaee6b7df3b25a1dfe
pcerimeli <mecacerimeli@gmail.com>
2020-02-24 18:35:27 -0300

    Programador

---
+++ propriedades.js
@@ -0,0 +1,12 @@
+const objeto  = {
+  nome: "Carlos",
+  idade: 32,
+
+tecnologias: [ 
+{nome: 'C++', especialidade: 'Desktop' },
+{ nome: 'Python', especialidade: 'Data Science' },
+{ nome: 'JavaScript', especialidade: 'Web/Mobile' }]
+
+}
+
+console.log(`O usuario ${objeto.nome} ${objeto.tecnologias[0].nome} com especialidade em ${objeto.tecnologias[0].especialidade}`)
