f59db33416ae01fb0b9ffe467efe8b82788addbe
pcerimeli <mecacerimeli@gmail.com>
2020-02-24 17:44:44 -0300

    endereço empresa

---
+++ empresa.js
@@ -0,0 +1,11 @@
+const empresa =  {
+  nome: "Rocketseat",
+  cor:"Roxo",
+  foco:"Programação",
+  endereco: {
+    rua:" Rua Guilherme Gembala"
+    ,numero:"260"
+  }  
+}
+
+console.log(`A empresa ${empresa.nome}esta localizada em${empresa.endereco.rua},${empresa.endereco.numero}`)
